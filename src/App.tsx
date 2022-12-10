d770773160e5c40758355c83b8fd288eae241086
Jake Saterlay <jakesaterlay@hotmail.co.uk>
2022-12-10 12:27:00 +0000

    basic formation picker implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import TeamViewer from "./components/TeamViewer";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p className="text-3xl font-bold">
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className="App h-screen bg-green-600">
+			<TeamViewer />
 		</div>
 	);
 }
---
+++ src/components/TeamViewer.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const TeamViewer = () => {
+	const [formation, setFormation] = useState("4-4-2");
+
+	const handleFormationChange = (
+		event: React.ChangeEvent<HTMLSelectElement>
+	) => {
+		setFormation(event.target.value);
+	};
+
+	return (
+		<React.Fragment>
+			<select
+				id="small"
+				className="block w-64 p-2 mb-6 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 "
+				onChange={handleFormationChange}
+			>
+				<option selected>Choose a formation</option>
+				<option value="4-4-2">4-4-2</option>
+				<option value="3-4-3">3-4-3</option>
+				<option value="4-3-3">4-3-3</option>
+				<option value="5-3-2">5-3-2</option>
+			</select>
+
+			<div className="h-full flex flex-col justify-center">
+				{formation.split("-").map((row, index) => (
+					<div className="flex flex-row justify-center" key={index}>
+						{Array(parseInt(row))
+							.fill(0)
+							.map((_, index) => (
+								<div className="h-24 w-24 bg-white m-10" key={index}></div>
+							))}
+					</div>
+				))}
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default TeamViewer;
